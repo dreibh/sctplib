ff0ba55ec20dacc96c889364860cd3a7317a71c3
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2007-01-19 16:19:48 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/sctplib1@1299 516812f1-d3fd-0310-9ea4-9aededebcb87

--- sctplib/sctp/globals.h
+++ sctplib/sctp/globals.h
@@ -135,7 +135,7 @@
                                    for instance abort of an association. */
 #define ERROR_FATAL        1    /* recovery from error was not possible, the program exits. */
 
-#define Current_error_log_ 0    /* Defines the level up to which the errors are printed.
+#define Current_error_log_ 1    /* Defines the level up to which the errors are printed.
                                    ERROR_WARNING (4) means all events are printed.
                                    This parameter could also come from a command line option */
 
