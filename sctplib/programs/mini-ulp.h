146cb7a93772dd0bc6e29f54d6da5ed6eea503a0
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2008-12-01 11:46:26 +0000

    Header updates.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/sctplib1@2047 516812f1-d3fd-0310-9ea4-9aededebcb87

--- sctplib/programs/mini-ulp.h
+++ sctplib/programs/mini-ulp.h
@@ -7,10 +7,10 @@
  * Realized in co-operation between Siemens AG
  * and University of Essen, Institute of Computer Networking Technology.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,11 +18,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * There are two mailinglists available at http://www.sctp.de which should be
- * used for any discussion related to this implementation.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Contact: discussion@sctp.de
  *          tuexen@fh-muenster.de
--- sctplib/programs/test_tool.c
+++ sctplib/programs/test_tool.c
@@ -1,20 +1,31 @@
-/***************************************************************************
-                                    test_tool.c
-                             --------------------
-    begin                : Fri Mar  2 09:43:00 CET 2001
-    copyright            : (C) 2001 by Andreas Lang
-    email                : anla@gmx.net
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
+/* $Id$
+ * --------------------------------------------------------------------------
+ *
+ *           //=====   //===== ===//=== //===//  //       //   //===//
+ *          //        //         //    //    // //       //   //    //
+ *         //====//  //         //    //===//  //       //   //===<<
+ *              //  //         //    //       //       //   //    //
+ *       ======//  //=====    //    //       //=====  //   //===//
+ *
+ * ---------------A SCTP implementation according to RFC 4960 ---------------
+ *
+ * Copyright (C) 2001 by Andreas Lang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: anla@gmx.net
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
