70e1009d5b3555702a6b5769e4d46b9119d32595
Thomas Dreibholz <dreibh@simula.no>
2018-10-03 15:29:36 +0100

    Updated autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,19 @@
-#!/bin/sh
-./bootstrap && ./configure --enable-static --disable-shared --enable-maintainer-mode --enable-sctp-over-udp $@ && make
+#!/bin/bash -e
+
+rm -f CMakeCache.txt
+
+# ------ Obtain number of cores ---------------------------------------------
+# Try Linux
+cores=`getconf _NPROCESSORS_ONLN 2>/dev/null || true`
+if [ "$cores" == "" ] ; then
+   # Try FreeBSD
+   cores=`sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2 | tr -d ' '`
+fi
+if [ "$cores" == "" ] ; then
+   cores="1"
+fi
+
+# ------ Configure and build ------------------------------------------------
+./bootstrap
+./configure --enable-static --enable-shared --enable-maintainer-mode --enable-sctp-over-udp $@
+make -j${cores}
